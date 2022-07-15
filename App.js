429bb7b10eea3a78db15bd2a5714302a22521843
jrslopez <lopez.jamesrussel@gmail.com>
2022-07-15 00:54:23 -0400

    Added request method

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-// console.log("Starting")
+const request = require("request")
 
-// setTimeout(() => {
-//   console.log("2 Second Timer")
-// }, 2000)
+const url =
+  "http://api.weatherstack.com/current?access_key=edd3110c01da9dcc5d1e3c537c23407e&query=37.8267,-122.4233&units=f"
 
-// setTimeout(() => {
-//   console.log("0 Second Timer")
-// }, 0)
-
-// console.log("Stopping")
-
-const listLocations = (locations) => {
-  locations.forEach((location) => {
-    console.log(location)
-  })
-}
-
-const myLocations = ["Philly", "NYC"]
-
-listLocations(myLocations)
+request({ url: url, json: true }, (error, response) => {
+  console.log(
+    response.body.current.weather_descriptions[0] +
+      ". It is currently " +
+      response.body.current.temperature +
+      " degrees out. It feels like " +
+      response.body.current.feelslike +
+      " degrees out."
+  )
+})
