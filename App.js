06ab9bddc03c9e02ae06d1f14596e0676f399084
jrslopez <lopez.jamesrussel@gmail.com>
2022-07-16 03:28:36 -0400

    command line search added

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 const geocode = require("./utils/Geocode")
 const forecast = require("./utils/Forecast")
 
-geocode("Philadelphia", (error, data) => {
-  console.log("Error", error)
-  console.log("Data", data)
-})
+const location = process.argv[2]
 
-forecast(44.1545, -75.7088, (error, data) => {
-  console.log("Error", error)
-  console.log("Data", data)
-})
+if (!location) {
+  console.log("Provide a valid location")
+} else {
+  geocode(location, (error, data) => {
+    if (error) {
+      return console.log(error)
+    }
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error)
+      }
+
+      console.log(data.location)
+      console.log(forecastData)
+    })
+  })
+}
